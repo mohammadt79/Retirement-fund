018eee7fe13ee3d2952e4f3fd15d69ca2adb2871
mohammadt <aria.persian.programer@gmail.com>
2022-08-28 18:39:42 +0430

    entityframeWorkFunction

--- Retirementfund_MVC/Controllers/HomeController.cs
+++ Retirementfund_MVC/Controllers/HomeController.cs
@@ -6,20 +6,27 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Retirementfund_MVC.Models;
+using Retirementfund_MVC.Data;
 
 namespace Retirementfund_MVC.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly DataContext _context;
+
 
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
         }
-
+        public  HomeController(DataContext context)
+        {
+            _context = context;
+        }
         public IActionResult Index()
         {
+           var s=_context.TheConferenceModel.Select(x=>x).ToList;
             return View();
         }
        
