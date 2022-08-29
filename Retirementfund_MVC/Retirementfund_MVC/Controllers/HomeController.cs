7404ba13b3c83037be9293f31bcae4b10028017e
mohammadt <aria.persian.programer@gmail.com>
2022-08-29 14:18:48 +0430

    change

---
+++ Retirementfund_MVC/Retirementfund_MVC/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Retirementfund_MVC.Models;
+using Retirementfund_MVC.Data;
+
+namespace Retirementfund_MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly DataContext _context;
+        public HomeController(ILogger<HomeController> logger,DataContext context)
+        {
+            _logger = logger;
+            _context = context;
+        }
+        //public void HomeService(DataContext context)
+        //{
+        //    _context = context;
+        //}
+
+        public IActionResult Index()
+        {
+           
+            ViewBag.Emp_data= _context.TheConferenceModel.ToList();
+            return View() ;
+        }
+       
+
+        public IActionResult RegisterRequest()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Retirementfund_MVC/Retirementfund_MVC/Migrations/20220829083919_001.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Retirementfund_MVC.Migrations
+{
+    public partial class _001 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ImgUrl",
+                table: "TheConferenceModel",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImgUrl",
+                table: "TheConferenceModel");
+        }
+    }
+}
